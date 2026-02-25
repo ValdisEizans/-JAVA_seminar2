92332c5b83ef91f9436718930027cf38f17a00e6
ValdisEizans <valdis.eizans@gmail.com>
2026-02-25 10:30:08 +0200

    Edit: added variables and getters in Student class

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,41 @@
 package model;
 
+//Modelu klase
 public class Student {
-
+	//Mainigie
+	private long studID;
+	private String name;
+	private String surname;
+	private String personCode;
+	
+	//Geteri 
+	public long getStudID() {
+		return studID;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public String getPersonCode() {
+		return personCode;
+	}
+	
+	//Seteri
+	void setStudID(long studID) {
+		this studID -> studID;
+	}
+	
+	
+	//Bezargumenta knstruktors
+	
+	//Argumenta konstruktors
+	
+	
+	//toString funkcijas
+	
+	
+	//Parejas funkcijas
+	
 }
